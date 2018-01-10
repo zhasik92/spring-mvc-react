c8f9ccf028d8d0a76c77204139d4d7af251b19c8
Zhasulan <zhasulan.kukebaev@atrinity>
2018-01-11 02:11:52 +0300

    moved from mysql to postgresql

--- src/main/java/com/mkyong/web/entity/User.java
+++ src/main/java/com/mkyong/web/entity/User.java
@@ -10,7 +10,7 @@ import java.util.Date;
 import java.util.Set;
 
 @Entity
-@Table(name = "user")
+@Table(name = "userr")
 public class User {
     @Id
     @GeneratedValue(generator = "increment")
--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -6,22 +6,19 @@ const defaults = {
   entry: './app/index.jsx',
   output: {
     path: path.join(__dirname, './resources/js'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /(node_modules|bower_components|public)/,
       loader: "babel"
-    }, {
-      test: /\.json$/,
-      loader: 'json-loader'
-    }],
+    }]
   },
   resolve: {
     modulesDirectories: ['node_modules'],
     root: path.resolve('./app'),
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['', '.js', '.jsx']
   },
   plugins: [
     // Avoid publishing files when compilation fails
@@ -29,14 +26,14 @@ const defaults = {
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery',
-      'window.jQuery': 'jquery',
-    }),
+      'window.jQuery': 'jquery'
+    })
     // new HtmlWebpackPlugin({
     //   template: path.join(__dirname, './app/index.html'),
     //   filename: 'index.html',
     //   inject: 'body'
     // })
-  ],
+  ]
 };
 
 module.exports = defaults;
